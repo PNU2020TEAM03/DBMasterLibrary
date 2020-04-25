d85b96461705acebdcafdded91c55c34aa6ef054
lsc013792 <lsc013792@gmail.com>
2020-04-25 11:03:29 +0900

    #DBMA-11
    #DBMA-11  / DB서버 연결 / 테스트 / 성능확인

--- DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/Connection.java
+++ DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/Connection.java
@@ -1,19 +1,48 @@
 package com.dbmasterlibrary;
 
+
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class Connection {
-    private String username;
-    private String pw;
-    private String URL;
+    // http://54.180.95.198:8081/
+    public void request(String urlStr) {
+        StringBuilder output = new StringBuilder();
 
-    public Connection(String name, String pw, String URL) {
-        this.username = username;
-        this.pw = pw;
-        this.URL = URL;
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        this.connect();
+            if (conn != null) {
+                conn.setConnectTimeout(5000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                int resCode = conn.getResponseCode();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String line = null;
+                while(true) {
+                    line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    output.append(line).append("\n");
+                }
+                reader.close();
+                conn.disconnect();
+            }
+
+        } catch (Exception ex) {
+            Log.d("예외 발생 : ", "request: " + ex.toString());
+        }
+        Log.d("Log", "request: " + output.toString());
+
     }
 
-    public void connect() {
 
-    }
 }
