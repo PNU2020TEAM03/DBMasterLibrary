2c702c8e7cf4b31d550157431f473f9fd17626d1
lsc013792 <lsc013792@gmail.com>
2020-04-25 11:30:22 +0900

    #DBMA-11
    #DBMA-11  / DB 서버 연결 /  라이브러리 사용 테스트 /  성능확인

--- DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/Connection.java
+++ DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/Connection.java
@@ -10,7 +10,7 @@ import java.net.URL;
 
 public class Connection {
     // http://54.180.95.198:8081/
-    // id pw 123
+    // id pw 
     public void request(String urlStr) {
         StringBuilder output = new StringBuilder();
 
