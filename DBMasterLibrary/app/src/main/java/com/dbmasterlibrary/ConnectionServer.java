b83d5df5af7871600b1c254aaab1f23a740b943c
lsc013792 <lsc013792@gmail.com>
2020-05-06 22:49:37 +0900

    #DBMA-77
    서버연결 함수에서 return 값이 null이 되는 오류 수정

--- DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/ConnectionServer.java
+++ DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/ConnectionServer.java
@@ -21,38 +21,32 @@ import okhttp3.Response;
 
 public class ConnectionServer {
 
-    public static class HttpAsyncTask extends AsyncTask<String, Void, String> {
 
+
+    public String connectionRequest(String userId, String userPw) throws JSONException, IOException {
+        String result = null;
         OkHttpClient client = new OkHttpClient();
 
-        @Override
-        protected String doInBackground(String... strings) {
+        //서버 주소
+        String baseUrl = "http://54.180.95.198:8081/dbmasterspringboot-1.0";
+        String strApi = "/v1/connection/check";
 
-            return null;
-        }
-
-        public void connectionRequest(String userId, String userPw) throws JSONException {
-
-            //OkHttpClient client = new OkHttpClient();
-
-            //서버 주소
-            String baseUrl = "http://54.180.95.198:8081/dbmasterspringboot-1.0";
-            String strApi = "/v1/connection/check";
-
-            //서버에 전송할 json객체 생성
-            final JSONObject object = new JSONObject();
-            object.put("name", userId);
-            object.put("pw", userPw);
+        //서버에 전송할 json객체 생성
+        final JSONObject object = new JSONObject();
+        object.put("name", userId);
+        object.put("pw", userPw);
 
 
-            Request request = new Request.Builder()
-                    .url(baseUrl + strApi)
-                    .post(RequestBody.create(MediaType.parse("application/json"), String.valueOf(object)))
-                    .build();
+        Request request = new Request.Builder()
+                .url(baseUrl + strApi)
+                .post(RequestBody.create(MediaType.parse("application/json"), String.valueOf(object)))
+                .build();
 
-            client.newCall(request).enqueue(connectionRequestCallback);
-        }
+        Response response =  client.newCall(request).execute();
 
+        return response.body().string();
+    }
+        /*
         private Callback connectionRequestCallback = new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -77,6 +71,6 @@ public class ConnectionServer {
             }
         };
 
-    }
+    }*/
 
 }
