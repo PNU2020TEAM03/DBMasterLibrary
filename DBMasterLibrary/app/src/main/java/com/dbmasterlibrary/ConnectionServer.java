790ba3daba908e0c9f7bf986bf91db5e5d53788a
lsc013792 <lsc013792@gmail.com>
2020-05-10 11:20:44 +0900

    #DBMA-88
    데이터베이스 테이블 생성함수의 불필요한 주석 제거 및 필요한 주석 입력

--- DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/ConnectionServer.java
+++ DBMasterLibrary/app/src/main/java/com/dbmasterlibrary/ConnectionServer.java
@@ -1,7 +1,5 @@
 package com.dbmasterlibrary;
 
-import android.util.Log;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -78,18 +76,15 @@ public class ConnectionServer {
                 .post(RequestBody.create(MediaType.parse("application/json"), String.valueOf(object)))
                 .build();
 
-        // 서버 결과 받아오기
         Response response = client.newCall(request).execute();
-
-        // 받아온 결과에서 body를 String 으로 변환
         String responseDataString = response.body().string();
-
-        //받아온 body를  json 객체로 변환
         JSONObject jsonObject = new JSONObject(responseDataString);
 
+        // E01 : 테이블 이름이 이미 있는 경우(테이블 생성 실패)
         if (jsonObject.getString("result").equals("E01")) {
             result = "table already exists";
         }
+        // S01 : 테이블 생성 성공
         if (jsonObject.getString("result").equals("S01")) {
             result = "create success";
         }
